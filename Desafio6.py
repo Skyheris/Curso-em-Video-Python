42b1ed664273781f69f3ae967be7cdacc0b294d6
Francisco Oliveira <142545580+Skyheris@users.noreply.github.com>
2023-08-17 23:23:23 +0100

    Update and rename Desafio6.py to Ex006.py

--- Desafio6.py
+++
@@ -1,2 +0,0 @@
-n = int(input("Digita um número inteiro: "))
-print ("O dobro de \033[4;31m{}\033[m é \033[34m{}\033[m, o seu triplo é \033[35m{}\033[m e a sua raiz quadrada é \033[33m{:.1f}\033[m".format(n, n*2, n*3, n**(1/2)))
