26be5440147f47c7d6e17003d8a97095f0a27a54
Francisco Oliveira <142545580+Skyheris@users.noreply.github.com>
2023-08-17 23:23:55 +0100

    Update and rename Desafio8.py to Ex008.py

--- Desafio8.py
+++
@@ -1,2 +0,0 @@
-m = float(input("Digita um valor em metros: "))
-print ("O valor que escreveste, {}, convertido em centímetros é {:.1f}cm e convertido em milímetros é {:.1f}mm".format(m, m*100, m*1000))
