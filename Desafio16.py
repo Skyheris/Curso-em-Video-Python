a21907f9162809f396f55958ec56cb5b940929de
Francisco Oliveira <142545580+Skyheris@users.noreply.github.com>
2023-08-17 23:26:56 +0100

    Update and rename Desafio16.py to Ex016.py

--- Desafio16.py
+++
@@ -1,4 +0,0 @@
-import math
-
-n = float(input("Digita um número: "))
-print(f"Do número que digitaste, {n}, a parte inteira é: {math.trunc(n)}")
---
+++ Ex016.py
@@ -0,0 +1,4 @@
+import math
+
+n = float(input("Digita um número: "))
+print(f"Do número que digitaste, {n}, a parte inteira é: {math.trunc(n)}")
