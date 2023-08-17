afaed02d7febe6e78cc55a1280b8c7739aa561c5
Francisco Oliveira <142545580+Skyheris@users.noreply.github.com>
2023-08-17 23:26:30 +0100

    Update and rename Desafio12.py to Ex012.py

--- Desafio12.py
+++
@@ -1,3 +0,0 @@
-p = float(input("Digita o preço de um produto: "))
-pn = p*0.95
-print(f"O produto que antes custava {p}€, agora custa {pn}€")
---
+++ Ex012.py
@@ -0,0 +1,3 @@
+p = float(input("Digita o preço de um produto: "))
+pn = p*0.95
+print(f"O produto que antes custava {p}€, agora custa {pn}€")
